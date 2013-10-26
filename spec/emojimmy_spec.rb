3315e788c4247e5618c325a42c9bdceeae18972b
Rémi Prévost <remi@exomel.com>
2013-10-26 09:17:44 -0400

    Use “text” in spec, not “comment”

--- spec/emojimmy_spec.rb
+++ spec/emojimmy_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 
 describe Emojimmy do
   describe :text_to_emoji do
-    let(:stored_comment) { 'Hello {U+1F601}{U+1F601} you {U+1F608} {U+1F438}!' }
-    let(:returned_comment) { 'Hello 😁😁 you 😈 🐸!' }
+    let(:stored_text) { 'Hello {U+1F601}{U+1F601} you {U+1F608} {U+1F438}!' }
+    let(:returned_text) { 'Hello 😁😁 you 😈 🐸!' }
 
     specify do
-      expect(Emojimmy.text_to_emoji(stored_comment)).to eql returned_comment
+      expect(Emojimmy.text_to_emoji(stored_text)).to eql returned_text
     end
   end
 
   describe :emoji_to_text do
-    let(:received_comment) { 'Hello 😁😁 you 😈 🐸!' }
-    let(:stored_comment) { 'Hello {U+1F601}{U+1F601} you {U+1F608} {U+1F438}!' }
+    let(:received_text) { 'Hello 😁😁 you 😈 🐸!' }
+    let(:stored_text) { 'Hello {U+1F601}{U+1F601} you {U+1F608} {U+1F438}!' }
 
     specify do
-      expect(Emojimmy.emoji_to_text(received_comment)).to eql stored_comment
+      expect(Emojimmy.emoji_to_text(received_text)).to eql stored_text
     end
   end
 end
