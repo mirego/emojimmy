cd5d4706e70fbb7534f16b393d97bcb65297c2b4
Rémi Prévost <remi@exomel.com>
2013-10-27 23:30:40 -0400

    Update Emojimmy specs

--- spec/emojimmy_spec.rb
+++ spec/emojimmy_spec.rb
@@ -6,17 +6,15 @@ describe Emojimmy do
     let(:stored_text) { 'Hello {U+1F601}{U+1F601} you {U+1F608} {U+1F438}!' }
     let(:returned_text) { 'Hello 😁😁 you 😈 🐸!' }
 
-    specify do
-      expect(Emojimmy.token_to_emoji(stored_text)).to eql returned_text
-    end
+    subject { Emojimmy.token_to_emoji(stored_text) }
+    it { should eql returned_text }
   end
 
   describe :emoji_to_token do
     let(:received_text) { 'Hello 😁😁 you 😈 🐸!' }
     let(:stored_text) { 'Hello {U+1F601}{U+1F601} you {U+1F608} {U+1F438}!' }
 
-    specify do
-      expect(Emojimmy.emoji_to_token(received_text)).to eql stored_text
-    end
+    subject { Emojimmy.emoji_to_token(received_text) }
+    it { should eql stored_text }
   end
 end
