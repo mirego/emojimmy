7f670ea1917541fe1a66f8126d9d6657be2775e6
Rémi Prévost <remi@exomel.com>
2013-11-08 09:24:19 -0500

    Add mega iOS emoji test

--- spec/emojimmy_spec.rb
+++ spec/emojimmy_spec.rb
@@ -11,10 +11,52 @@ describe Emojimmy do
   end
 
   describe :emoji_to_token do
-    let(:received_text) { 'Hello 😁😁 you 😈 🐸!' }
-    let(:stored_text) { 'Hello :grin::grin: you :smiling_imp: :frog:!' }
+    context 'with a few emoji characters' do
+      let(:received_text) { 'Hello 😁😁 you 😈 🐸!' }
+      let(:stored_text) { 'Hello :grin::grin: you :smiling_imp: :frog:!' }
 
-    subject { Emojimmy.emoji_to_token(received_text) }
-    it { should eql stored_text }
+      subject { Emojimmy.emoji_to_token(received_text) }
+      it { should eql stored_text }
+    end
+
+    context 'with *all* iOS emoji' do
+      let(:received_text) do
+        '😄😃😀😊☺😉😍😘😚😗😙😜😝😛😳😁😔😌😒😞😣
+        😢😂😭😪😥😰😅😓😩😫😨😱😠😡😤😖😆😋😷😎😴
+        😵😲😟😦😧😈👿😮😬😐😕😯😶😇😏😑👲👳👮👷💂
+        👶👦👧👨👩👴👵👱👼👸😺😸😻😽😼🙀😿😹😾👹👺
+        🙈🙉🙊💀👽💩🔥✨🌟💫💥💢💦💧💤💨👂👀👃👅👄
+        👍👎👌👊✊✌👋✋👐👆👇👉👈🙌🙏☝👏💪🚶🏃💃
+        👫👪👬👭💏💑👯🙆🙅💁🙋💆💇💅👰🙎🙍🙇🎩👑👒
+        👟👞👡👠👢👕👔👚👗🎽👖👘👙💼👜👝👛👓🎀🌂💄
+        💛💙💜💚❤💔💗💓💕💖💞💘💌💋💍💎👤👥💬👣💭
+        🐶🐺🐱🐭🐹🐰🐸🐯🐨🐻🐷🐽🐮🐗🐵🐒🐴🐑🐘🐼🐧
+        🐦🐤🐥🐣🐔🐍🐢🐛🐝🐜🐞🐌🐙🐚🐠🐟🐬🐳🐋🐄🐏
+        🐀🐃🐅🐇🐉🐎🐐🐓🐕🐖🐁🐂🐲🐡🐊🐫🐪🐆🐈🐩🐾
+        💐🌸🌷🍀🌹🌻🌺🍁🍃🍂🌿🌾🍄🌵🌴🌲🌳🌰🌱🌼🌐
+        🌞🌝🌚🌑🌒🌓🌔🌕🌜🌛🌙🌍🌎🌏🌋🌌🌠⭐
+        ☀⛅☁⚡☔❄⛄🌀🌁🌈🌊
+        🏠🏡🏫🏢🏣🏥🏦🏪🏩🏨💒⛪🏬🏤🌇🌆🏯🏰⛺🏭🗼
+        🗾🗻🌄🌅🌃🗽🌉🎠🎡⛲🎢🚢⛵🚤🚣⚓🚀✈💺🚁🚂
+        🚊🚉🚎🚆🚄🚅🚈🚇🚝🚋🚃🚎🚌🚍🚙🚘🚗🚕🚖🚛🚚
+        🚨🚓🚔🚒🚑🚐🚲🚡🚟🚠🚜💈🚏🎫🚦🚥⚠🚧🔰⛽🏮
+        🎰♨🗿🎪🎭📍🚩🇯🇵🇰🇷🇩🇪🇨🇳🇺🇸🇫🇷🇪🇸🇮🇹🇷🇺🇬🇧
+        1⃣2⃣3⃣4⃣5⃣6⃣7⃣8⃣9⃣0⃣🔟🔢#⃣🔣⬆⬇⬅➡🔠🔡🔤
+        ↗↖↘↙↔↕🔄◀▶🔼🔽↩↪ℹ⏪⏩⏫⏬⤵⤴🆗
+        🔀🔁🔂🆕🆙🆒🆓🆖📶🎦🈁🈯🈳🈵🈴🈲🉐🈹🈺🈶🈚
+        🚻🚹🚺🚼🚾🚰🚮🅿♿🚭🈷🈸🈂Ⓜ🛂🛄🛅🛃🉑㊙㊗
+        🆑🆘🆔🚫🔞📵🚯🚱🚳🚷🚸⛔✳❇❎✅✴💟🆚📳📴
+        🅰🅱🆎🅾💠➿♻♈♉♊♋♌♍♎♏♐♑♒♓⛎🔯
+        🏧💹💲💱©®™❌‼⁉❗❓❕❔⭕🔝🔚🔙🔛🔜🔃
+        🕛🕧🕐🕜🕑🕝🕒🕞🕓🕟🕔🕠🕕🕡🕖🕢🕗🕣🕘🕤🕙
+        🕥🕚🕦✖➕➖➗♠♥♣♦💮💯✔☑🔘🔗➰〰〽🔱
+        ◼◻◾◽▪▫🔺🔲🔳⚫⚪🔴🔵🔻⬜⬛🔶🔷🔸🔹
+        👎👊❔👌💛💢❔👊💙🌟❕
+        👀👃👅👂👄'
+      end
+
+      let(:stored_text) { Emojimmy.emoji_to_token(received_text) }
+      it { expect(stored_text.each_codepoint.to_a.max).to be <= 65535 }
+    end
   end
 end
